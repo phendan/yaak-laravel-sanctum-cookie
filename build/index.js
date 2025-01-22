9c11e55c93880e3c43f8458d59731a9ee682bbf9
Philip Braunen <pbraunen@gmail.com>
2025-01-22 19:19:45 +0100

    feat: implement laravel CSRF cookie template function

--- build/index.js
+++ build/index.js
@@ -24,17 +24,47 @@ __export(src_exports, {
 });
 module.exports = __toCommonJS(src_exports);
 var plugin = {
-  templateFunctions: [],
-  httpRequestActions: [
+  templateFunctions: [
     {
-      key: "example-plugin",
-      label: "Hello, From Plugin",
-      icon: "cake",
-      async onSelect(ctx, args) {
-        ctx.toast.show({
-          color: "success",
-          message: `You clicked the request ${args.httpRequest.name}`
+      name: "laravel_csrf_cookie",
+      description: "Retrieve the CSRF Cookie from Laravel Sanctum",
+      args: [
+        {
+          type: "http_request",
+          name: "request",
+          label: "Sanctum Cookie Request"
+        },
+        {
+          type: "text",
+          name: "name",
+          label: "Sanctum Cookie Name",
+          defaultValue: "XSRF-TOKEN",
+          placeholder: 'Laravel uses "XSRF-TOKEN" by default.'
+        }
+      ],
+      async onRender(ctx, args) {
+        const requestId = args.values.request;
+        const cookieName = args.values.name ?? "XSRF-TOKEN";
+        if (typeof requestId === "undefined") return null;
+        const httpRequest = await ctx.httpRequest.getById({ id: requestId });
+        if (httpRequest === null) return null;
+        const response = await ctx.httpRequest.send({ httpRequest });
+        if (response.error || response.status === 0) {
+          const message = `Failed to send dependent request: ${httpRequest?.name}`;
+          ctx.toast.show({ color: "warning", message });
+          return null;
+        }
+        const cookie = response.headers.find((it) => {
+          return (it.name === "Set-Cookie" || it.name === "set-cookie") && it.value.includes(cookieName);
         });
+        if (typeof cookie === "undefined") {
+          const message = `There was no ${cookieName} header present on the response.`;
+          ctx.toast.show({ color: "warning", message });
+          return null;
+        }
+        const pattern = new RegExp(`(?<=${cookieName}=)[^;]+`, "g");
+        const token = cookie.value.match(pattern)[0];
+        return decodeURIComponent(token);
       }
     }
   ]
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import {plugin} from "./index";
+import { plugin } from './index';
 
 describe('Example Plugin', () => {
     test('Exports plugin object', () => {
